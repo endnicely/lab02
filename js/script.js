e456a10850995b3da525cfd9da25c8c905fbf3c6
endnicely <zhoujie66@yahoo.com>
2018-02-06 20:57:31 -0800

    New file added.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*eslint-env browser*/
+var milesDriven = window.prompt("How many miles have you driven?");
+var gallons = window.prompt("How many gallons of fuel does your car hold?");
+var mpg = milesDriven / gallons;
+document.write(parseInt(mpg, 10));
